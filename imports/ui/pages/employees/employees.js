a5598356e8a52fdda4363b992b0960743b80583b
MWight20 <mwight20@gmail.com>
2018-04-06 21:43:24 -0500

    Add files via upload

--- imports/ui/pages/employees/employees.js
+++ imports/ui/pages/employees/employees.js
@@ -1,7 +1,25 @@
-import { Users } from '../../../api/users/users.js';
-
+import { Users } from '/imports/api/users/users.js';
+import { Meteor } from 'meteor/meteor';
 import './employees.html';
 
 // TODO
 //   Integrate with Database
 //   Get Employees Logic
+
+Template.employees.onCreated(function () {
+    Meteor.subscribe('users.all');
+  });
+
+Template.employees.helpers({
+    users() {
+      return Meteor.users.find({});
+    },
+  });
+
+  Template.employees.events({
+    'click .addnewmember': function(event){
+      event.preventDefault();
+      FlowRouter.go('/newEmployee');
+    }
+  });
+
