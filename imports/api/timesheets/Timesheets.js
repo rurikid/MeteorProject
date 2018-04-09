b5506fa3ffe8413e769998e1e000b7f2fef08ed2
rurikid <30425208+rurikid@users.noreply.github.com>
2018-04-09 13:45:30 -0500

    Rename Timesheets.js to timesheets.js

--- imports/api/timesheets/Timesheets.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
- 
-export const Timesheets = new Mongo.Collection('Timesheets');
---
+++ imports/api/timesheets/timesheets.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+ 
+export const Timesheets = new Mongo.Collection('Timesheets');
