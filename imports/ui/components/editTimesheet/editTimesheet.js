72362424657fed874cf24a4605a418c56a964e5b
rurikid <30425208+rurikid@users.noreply.github.com>
2018-04-26 17:11:15 -0500

    deprecated

--- imports/ui/components/editTimesheet/editTimesheet.js
+++
@@ -1,9 +0,0 @@
-import { Users } from '../../../api/users/users.js';
-import { Projects } from '../../../api/projects/projects.js'
-
-import './editTimesheet.html'
-
-// TODO
-// 	Integrate with Database
-// 	Select Timeslot Logic
-// 	Update Timesheet Logic
